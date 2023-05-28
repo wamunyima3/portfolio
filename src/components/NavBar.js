91a898ef7281bdcaa2b2731a9c096184dc28b616
wamunyima3 <wamunyimamukelabai3@gmail.com>
2023-05-28 18:02:19 +0200

    adding media queries for navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './NavBar.css'
 import { CgSun } from "react-icons/cg";
 import { HiMoon } from "react-icons/hi";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
+  const [open,setOpen] = useState(false);
+  const openMenu = () => setOpen(!open);
+
   return (
     <div id='nav-bar'>
         <h1 id='logo'><Link to='/'>Portifolio</Link></h1>
-        <ul id='nav-links'>
-            <li><Link to='/'>Home</Link> </li>
+        <ul id={open ? 'nav-links active':'nav-links'}>
+            <li><Link to='/' className='active'>Home</Link> </li>
             <li><Link to='/'>About</Link> </li>
             <li><Link to='/'>Services</Link> </li>
             <li><Link to='/'>Projects</Link> </li>
@@ -19,6 +23,9 @@ const NavBar = () => {
             <HiMoon/>
             <CgSun/>
             <div id='themeTogoButton'></div>
+        </div>
+        <div id='nav-menu-icon' onClick={openMenu}>
+          {open ? (<FaTimes/>) : (<FaBars/>)}
         </div>
     </div>
   )
