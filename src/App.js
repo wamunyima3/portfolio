87d123c0e4f218bb23e6cf213532c54fdc120793
wamunyima3 <wamunyimamukelabai3@gmail.com>
2023-06-11 16:55:48 +0200

    Started working on the hero section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
         <NavBar/>
+        <Hero/>
     </>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import heroImage from './asserts/images/heroImage.jpg';
+
+const Hero = () => {
+  return (
+    <div id='hero'>
+      <div id="hero-text">
+        <h4>Welcome! My name is</h4>
+        <h1>Wamunyima Mukelabai</h1>
+        <h5>A software engineer skilled in</h5>
+      </div>
+      <img id='hero-image' alt="Wamunyima's image" src={heroImage} />
+    </div>
+  );
+}
+
+export default Hero;
