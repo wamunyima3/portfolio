82034c95adfc6bc9ecd5d08967103b49294ac9de
wamunyima3 <wamunyimamukelabai3@gmail.com>
2023-06-11 20:53:00 +0200

    Fixed the nav-bar

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import heroImage from './asserts/images/heroImage.jpg';
 
 const Hero = () => {
   return (
@@ -9,7 +8,6 @@ const Hero = () => {
         <h1>Wamunyima Mukelabai</h1>
         <h5>A software engineer skilled in</h5>
       </div>
-      <img id='hero-image' alt="Wamunyima's image" src={heroImage} />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,13 +11,13 @@ const NavBar = () => {
   return (
     <div id='nav-bar'>
         <h1 id='logo'><Link to='/'>Portifolio</Link></h1>
-        <ul id={open ? 'nav-links active':'nav-links'}>
-            <li><Link to='/' className='active'>Home</Link> </li>
-            <li><Link to='/'>About</Link> </li>
-            <li><Link to='/'>Services</Link> </li>
-            <li><Link to='/'>Projects</Link> </li>
-            <li><Link to='/'>Contact</Link> </li>
-        </ul>
+        <nav className={open ? 'nav-links active':'nav-links'}>
+            <Link to='/' className='active'>Home</Link>
+            <Link to='/'>About</Link>
+            <Link to='/'>Services</Link>
+            <Link to='/'>Projects</Link>
+            <Link to='/'>Contact</Link>
+        </nav>
         <ThemeTogo/>
         <div id='nav-menu-icon' onClick={openMenu}>
           {open ? (<FaTimes/>) : (<FaBars/>)}
