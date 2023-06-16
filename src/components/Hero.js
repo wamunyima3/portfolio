3ed0fc495e90ed62c837e2edc598c7e34f4cf919
wamunyima3 <wamunyimamukelabai3@gmail.com>
2023-06-16 17:39:22 +0200

    Started making the hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,24 @@
 import React from 'react';
+// import HeroImage from '../asserts/hero-image1.png'
+import HeroImage from '../asserts/hero_two-final.png'
+import './Hero.css';
+import { FaFacebookF, FaGithub } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
     <div id='hero'>
       <div id="hero-text">
-        <h4>Welcome! My name is</h4>
+        <h3>Welcome! My name is</h3>
         <h1>Wamunyima Mukelabai</h1>
-        <h5>A software engineer skilled in</h5>
+        <h2>A software engineer skilled in mobile development</h2>
+        <div id="socials">
+          <Link to='/'><FaFacebookF/></Link>
+          <Link to='/'><FaGithub/></Link>
+        </div>
+        <Link className='btn' to='/'>Download CV</Link>
       </div>
+      <img alt='HeroImage' src={HeroImage}/>
     </div>
   );
 }
