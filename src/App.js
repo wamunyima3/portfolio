fb7eae1f6df187e1bd6d550631da77be076a83d7
wamunyima3 <wamunyimamukelabai3@gmail.com>
2023-06-28 21:44:48 +0200

    Created a contact section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Services from "./components/Services";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   // if we want multiple pages we can create routes
@@ -11,6 +12,7 @@ function App() {
         <Hero/>
         <Services/>
         <Projects/>
+        <Contact/>
     </>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div id='contact'>
+        <h2>Contact Me</h2>
+        <form>
+            <div className='input-box'>
+                <input type='text' placeholder='Fullname'/>
+                <input type='email' placeholder='Email Address'/>
+            </div>
+            <div className='input-box'>
+                <input type='number' placeholder='Mobile number'/>
+                <input type='text' placeholder='Email Subject'/>
+            </div>
+            <textarea name='' id='' cols='30' rows='10' placeholder='Your message'></textarea>
+            <input type='submit' value='Send Message' className='btn'/>
+        </form>
+    </div>
+  )
+}
+
+export default Contact
