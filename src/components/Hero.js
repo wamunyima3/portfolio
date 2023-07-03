c88a5eec180623e2553214bc5969c243026346fb
wamunyima3 <wamunyimamukelabai3@gmail.com>
2023-07-03 19:16:22 +0200

    Typed.js now working

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,26 +1,48 @@
-import React from 'react';
-// import HeroImage from '../asserts/hero-image1.png'
-import HeroImage from '../asserts/hero_two-final.png'
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
+import HeroImage from '../asserts/hero_two-final.png';
 import './Hero.css';
-import { FaFacebookF, FaGithub } from "react-icons/fa";
+import { FaFacebookF, FaGithub } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Hero = () => {
+  const typedRef = useRef(null);
+  useEffect(() => {
+    const options = {
+      strings: [
+        'application development',
+        'web development',
+        'desktop development',
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+    };
+    // Initialize Typed.js
+    const typed = new Typed(typedRef.current, options);
+
+    // Clean up the Typed instance
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div id='hero'>
-      <div id="hero-text">
+      <div id='hero-text'>
         <h3>Welcome! My name is</h3>
         <h1>Wamunyima Mukelabai</h1>
-        <h2>A software engineer skilled in mobile development</h2>
-        <div id="socials">
-          <Link to='/'><FaFacebookF/></Link>
-          <Link to='/'><FaGithub/></Link>
+        <h2>A software engineer skilled in <span ref={typedRef}></span></h2>
+        <div id='socials'>
+          <Link to='/'><FaFacebookF /></Link>
+          <Link to='/'><FaGithub /></Link>
         </div>
         <Link className='btn' to='/'>Download CV</Link>
       </div>
-      <img alt='HeroImage' src={HeroImage}/>
+      <img alt='HeroImage' src={HeroImage} />
     </div>
   );
-}
+};
+
 
 export default Hero
