5006a4108a2be638d3e9bb1b0ede721ed38db81f
wamunyima3 <wamunyimamukelabai3@gmail.com>
2023-07-14 22:17:03 +0200

    Made theme consistent and added regex

--- src/components/ThemeTogo.js
+++ src/components/ThemeTogo.js
@@ -1,25 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { CgSun } from 'react-icons/cg';
 import { HiMoon } from 'react-icons/hi';
 import { themeContext } from './ThemeContext';
 
 const ThemeTogo = () => {
-
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
 
   const changeTheme = () => {
-    theme.dispatch({type:'toggle'})
-  }
-  
+    theme.dispatch({ type: 'toggle' });
+  };
+
+  useEffect(() => {
+    const storedMode = localStorage.getItem('themeMode');
+    if (storedMode) {
+      theme.dispatch({ type: 'setMode', payload: storedMode === 'dark' });
+    }
+  }, []);
+
   return (
     <div id='themeTogo' onClick={changeTheme}>
-      <HiMoon/>
-      <CgSun/>
-      <div id='themeTogoButton' style={darkMode?{left:'2px'}:{right:'2px'}} ></div>
+      <HiMoon />
+      <CgSun />
+      <div id='themeTogoButton' style={darkMode ? { left: '2px' } : { right: '2px' }}></div>
     </div>
   );
-
 };
 
 export default ThemeTogo;
